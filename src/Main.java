93507d80ee74891b2b9b96cc429112e74eb9f9db
osvaldinho11 <joaovitorperico@gmail.com>
2024-08-21 20:46:16 -0300

    Criação do projeto java

--- src/Main.java
+++ src/Main.java
@@ -2,5 +2,24 @@
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
     public static void main(String[] args) {
+
+        Integer idadePessoa = 20;
+        String nomePessoa ="Taffezinho";
+
+        // Operadores logicos
+        // > maior
+        // >= maior igual
+        //<  menor
+        //<= menor igual
+        //== é igual
+        //!= é diferente
+        //equal compara textos
+
+        if (idadePessoa > 21) {
+            System.out.println("É maior que 21");
+        }else{
+            System.out.println("Não é maior");
+        }
+
     }
 }
